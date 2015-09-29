137a39838d28623ed90a39ef7c47ad1b05fd0eb2
Dubpub <dubpub@loneboat.com>
2015-09-29 01:19:51 -0400

    Added CTM remover. Fixed Luther related bugs.

--- data/scripts/Luther.lua
+++ data/scripts/Luther.lua
@@ -26,10 +26,10 @@ end
 function HuntForPrisoner()
     local hunted = this.GetNearbyObjects("Prisoner", 100);
     local size = tablelength(hunted);
-    local picked = math.random(1, size);
-    local count = 1;
     local target;
     if size > 0 then
+        local picked = math.random(1, size);
+        local count = 1;
         for Prisoner, _ in next, hunted do
             -- Did we find him?
             if count == picked then
@@ -63,19 +63,29 @@ function HuntForPrisoner()
     return target;
 end
 
-function ScareNearbyGuards( injure )
-    local guards = this.GetNearbyObjects("Guard", 75);
+function ScareNearbyGuards()
+    local guards = this.GetNearbyObjects("Guard", 10);
     if tablelength(guards) > 0 then
         for Guard, _ in next, guards do
-            -- Push the guards away.
-            Guard.Damage = 0.85;
-            Guard.Energy = 0;
+            local eventID = math.random(1, 3);
+            if eventID == 1 then
+                -- RUN AWAY!
+                Guard.ClearRouting();
+                Guard.NavigateTo(this.Pos.x + 10, this.Pos.y);
+            elseif eventID == 2 then
+                -- Teleport to front of prison property
+                Guard.Pos.x = 50;
+                Guard.Pos.y = 50;
+            else
+               -- Knock out guard.
+                Guard.Damage = 0.85;
+            end
         end
     end
 end
 
 function Create()
-    Object.Sound("_Contraband", "SearchPrisoner");
+
 end
 
 function Update()
@@ -90,33 +100,64 @@ end
 function Act( elapsedTime )
     -- Get a victim
     local target = HuntForPrisoner();
-    -- Teleport to victim
-    this.Pos.x = target.Pos.x;
-    this.Pos.y = target.Pos.y;
+    if target ~= nil then
+        -- Teleport to victim
+        this.Pos.x = target.Pos.x;
+        this.Pos.y = target.Pos.y;
 
-    local hork = math.random(1,15);
-    if hork > 1 and hork < 5 then
-        -- Hurt
-        target.Damage = 0.5;
-    elseif hork > 5 and hork < 9 then
-        -- Give them a status effect
-        target.StatusEffects.virusdeadly = 1000;
-    elseif hork > 9 and hork < 13 then
-        target.StatusEffects.overdosed = 50;
-    elseif hork == 14 then
-        target.StatusEffects.wounded = 50;
-    elseif hork == 15 then
-        -- If kill, scare the guards.
-        -- Kill
-        target.Damage = 1;
+        local hork = math.random(1,15);
+        if hork > 1 and hork < 5 then
+            -- Hurt
+            target.Damage = 0.5;
+        elseif hork > 5 and hork < 9 then
+            -- Give them a status effect
+            target.StatusEffects.virusdeadly = 1000;
+        elseif hork > 9 and hork < 13 then
+            target.StatusEffects.overdosed = 50;
+        elseif hork == 14 then
+            target.StatusEffects.wounded = 50;
+        elseif hork == 15 then
+            -- If kill, scare the guards.
+            -- Kill
+            target.Damage = 1;
+        end
+
+        local eventID = math.random(1,4);
+        if eventID == 1 then
+            Game.DebugOut("Scaring nearby guards!");
+            ScareNearbyGuards();
+        elseif eventID == 2 then
+            Game.DebugOut("Spawn new, angry, inmates!");
+            local count = 0;
+            repeat
+                local Prisoner = Object.Spawn("Prisoner", this.Pos.x, this.Pos.y);
+                Prisoner.StatusEffects.angst = 2500;
+                Prisoner.StatusEffects.suppressed  = 0;
+                Prisoner.BoilingPoint = 100;
+                count = count + 1;
+            until(count == hork);
+        elseif eventID == 3 then
+            Game.DebugOut("Spawn some tasty contraband.");
+            local tempGuard = Object.Spawn("ArmedGuard", this.Pos.x, this.Pos.y);
+            local tempDoc = Object.Spawn("Doctor", this.Pos.x, this.Pos.y);
+            if tempGuard ~= nil and tempDoc ~= nil then
+                tempGuard.Damage = 0.85;
+                tempGuard.Delete();
+
+                tempDoc.Damage = 0.85;
+                tempDoc.Delete();
+            end
+        elseif eventID == 4 then
+            Game.DebugOut("Set all status effects to rioting!");
+            local prisoners = this.GetNearbyObjects("Prisoner", 50);
+            for Prisoner, _ in next, prisoners do
+                local misb = Prisoner.Misbehavior;
+                if misb ~= nil then
+                    Prisoner.StatusEffects.rioting = 1;
+                end
+            end
+        end
     end
-
-    local scareFactor = math.random(1,2);
-    if scareFactor == 1 then
-        Game.DebugOut("Scaring nearby guards!");
-        ScareNearbyGuards( scareFactor );
-    end
-
     if KillDelay == KillTime then
         this.Delete();
     end
---
+++ data/scripts/RemoveCTMEntities.lua
@@ -0,0 +1,44 @@
+--
+-- Author: Dubpub
+-- Date: 9/28/2015
+-- Time: 10:55 PM
+--
+local CTMList = {
+    "AssignBodyGuard",
+    "CallAllGuardsHere",
+    "CureAllPrisoners",
+    "InfectPatientZero",
+    "Luther",
+    "MakeAllAngry",
+    "OpenAllDoors",
+    "PlaceRandomContraband",
+    "RemoveAllFires",
+    "RestAllGuards",
+    "SatisfyAllNeeds",
+    "SpawnInmate",
+    "Spawn2Inmates",
+    "Spawn3Inmates",
+    "SpawnFire",
+    "StopEscapingPrisoners",
+    "TunnelDectector",
+}
+
+function tablelength(T)
+    local count = 0
+    for _ in pairs(T) do count = count + 1 end
+    return count
+end
+
+function Create()
+    for _, type in ipairs(CTMList) do
+        Game.DebugOut("Type: " .. type);
+        local EntityType = this.GetNearbyObjects(type, 1000);
+        if tablelength(EntityType) > 0 then
+            for Entity, _ in next, EntityType do
+                Entity.Delete();
+            end
+        end
+    end
+    this.Delete();
+end
+
